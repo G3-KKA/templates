2afe0a66999ea2aa5d30b73e602d72833d1d5488
GE:KKA <kosserdyuk@gmail.com>
2024-06-20 17:32:03 +0500

    rewrited logger template

--- logging/zap/zap.go
+++ logging/zap/zap.go
@@ -1,98 +1,74 @@
 package zapp
 
 import (
-	"encoding/json"
 	"log"
 	"os"
-	"strings"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-const (
-	PRODUCTION_FROM_CONFIG  = true
-	DEVELOPMENT_FROM_CONFIG = false
-)
-const PATH_TO_LOG_FILE_FROM_CONFIG = "./tmp/logs"
+var globalLogger *zap.SugaredLogger
 
-func InitLogger() (*zap.SugaredLogger, error) {
+type LevelWithName struct {
+	zap.AtomicLevel
+	Name string
+}
+
+func withName(name string, level zap.AtomicLevel) LevelWithName {
+	return LevelWithName{level, name}
+}
+func InitLogger() (*zap.SugaredLogger, []LevelWithName) {
 	log.Println("Logger initialization started")
+
+	// Creating encoders
 	encoder := setEncoder()
 
-	logfile, err := os.OpenFile(PATH_TO_LOG_FILE_FROM_CONFIG, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
-	if err != nil {
-		log.Println("Cannot create log file", err)
-		log.Println("Trying to create directory")
-		os.Mkdir(PATH_TO_LOG_FILE_FROM_CONFIG[:strings.LastIndex(PATH_TO_LOG_FILE_FROM_CONFIG, "/")], 0777)
-		logfile, err = os.OpenFile(PATH_TO_LOG_FILE_FROM_CONFIG, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
-		if err != nil {
-			log.Println("Unsuccessful logger initialization, cannot create log file ", err)
-			return nil, err
-		}
+	// Creating log file
+	logfile := getLogFile()
+
+	// Creating levels to be able to change level in runtime
+	levels := []LevelWithName{
+		withName("stdout", zap.NewAtomicLevelAt(zapcore.InfoLevel)),   // 0
+		withName("logfile", zap.NewAtomicLevelAt(zapcore.DebugLevel)), // 1
 	}
-	//Creating zap.Cores
-	//And merging them
-	logfilecore := zapcore.NewCore(encoder, logfile, zapcore.InfoLevel)
-	stdout := zapcore.NewCore(encoder, os.Stdout, zapcore.InfoLevel)
-	stderr := zapcore.NewCore(encoder, os.Stderr, zapcore.DebugLevel)
-	core := zapcore.NewTee(stdout, logfilecore, stderr)
-	//Creating Logger
-	//And sugaring
+
+	// Creating zap.Cores
+	// And merging them
+	logfilecore := zapcore.NewCore(encoder, logfile, levels[0])
+	stderr := zapcore.NewCore(encoder, os.Stderr, levels[1])
+	core := zapcore.NewTee(logfilecore, stderr)
+
+	// Creating Logger from cores
+	// And sugaring
+
 	logger := zap.New(core)
 	sugarlogger := logger.Sugar()
-	//First log message
-	//That tells us that logger construction succeeded
+
+	// First log message
+	// That tells us that logger construction succeeded
 	defer sugarlogger.Sync()
 	sugarlogger.Debug("Logger construction succeeded")
 
-	return sugarlogger, nil
+	return sugarlogger, levels
 }
-func setEncoder() zapcore.Encoder {
-	var encoder zapcore.Encoder
-	if PRODUCTION_FROM_CONFIG {
-		encoder = zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
 
-	} else if DEVELOPMENT_FROM_CONFIG {
-		encoder = zapcore.NewJSONEncoder(zap.NewDevelopmentEncoderConfig())
-	}
-	return encoder
+// Useful for small apps where you want to log a bit
+// Not sure about async
+func InitGlobalLogger() {
+	//Ignoring ability to change level in runtime for global usecase
+	//TODO: it is not hard to add this feature, mb next time
+	globalLogger, _ = InitLogger()
 }
-func __DEPRECATED__InitLogger() *zap.SugaredLogger {
-	// For some users, the presets offered by the NewProduction, NewDevelopment,
-	// and NewExample constructors won't be appropriate. For most of those
-	// users, the bundled Config struct offers the right balance of flexibility
-	// and convenience. (For more complex needs, see the AdvancedConfiguration
-	// example.)
-	//
-	// See the documentation for Config and zapcore.EncoderConfig for all the
-	// available options.
-	rawJSON := []byte(`{
-	  "level": "debug",
-	  "encoding": "json",
-	  "outputPaths": ["stdout", "./tmp/logs"],
-	  "errorOutputPaths": ["stderr"],
-	  "encoderConfig": {
-	    "levelEncoder": "lowercase"
-	  }
-	}`)
-	/*
-		"initialFields": {"foo": "bar"},
-		just key:value that will be added to every log record
-	*/
 
-	var cfg zap.Config
-	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
-		panic(err)
-	}
+// Logs to stderr
+func Debug(args ...any) {
+	globalLogger.Debug(args...)
+	globalLogger.Sync()
+}
 
-	logger, err := cfg.Build()
-	sugerlogger := logger.Sugar()
-	if err != nil {
-		panic(err)
-	}
-	defer logger.Sync()
-
-	sugerlogger.Info("logger construction succeeded")
-	return sugerlogger
+// Logs to stderr and logfile
+func Info(args ...any) {
+	globalLogger.Info(args...)
+	globalLogger.Sync()
 }
